d150c5996eaee6b93828d64b8019448dc22b9961
RayShaw <784452262@qq.com>
2016-11-23 15:10:11 +0800

    update

--- app.js
+++ app.js
@@ -14,8 +14,11 @@ db.once('open', function() {
     //console.log('connect')
 });
 
+// 去除 DeprecationWarning mpromise 警告
+// mongoose.Promise = global.Promise;
+
 //申明一个mongoons对象 Schema 文本属性对象
-var StudentSchema = mongoose.Schema({
+var studentSchema = mongoose.Schema({
     no: Number,
     name: String,
     age: Number,
@@ -25,32 +28,60 @@ var StudentSchema = mongoose.Schema({
     }
 })
 
-// 为Schema创建方法
-StudentSchema.methods.speak = function() {
+// 为Schema创建实例方法
+studentSchema.methods.speak = function() {
     var greeting = this.name ? "My name is " + this.name : "I don't have a name"
     console.log(greeting)
 }
 
-// 发布一个Model数据库模型
-var StudentModel = mongoose.model('Students', StudentSchema)
+// 创建实例方法 实例方法在Entity使用
+studentSchema.methods.findbyno = function(no, callback) {
+    return this.model('studentModel').find({ no: no }, callback);
+}
 
-// 用Model创建Entity实体
-var studentEntity = new StudentModel({ name: 'aaaa' })
-// console.log(studentEntity.name)
-studentEntity.speak();
-// studentEntity.save(function(err, studentEntity){
-//   if(err) return console.error(err);
-//   studentEntity.speak();
+// 创建静态方法 静态方法在Model使用
+studentSchema.statics.findbyname = function(name, callback) {
+    return this.model('studentModel').find({ name: name }, callback);
+}
+
+// 发布一个Model数据库模型 stu collection name
+var studentModel = mongoose.model('studentModel', studentSchema, 'stu')
+
+// 用Model创建Entity实例
+var newStu = { no: 1, name: 'aaaa', age: '10' };
+var studentEntity = new studentModel(newStu)
+// studentEntity.speak();
+
+// add new student 基于实例entiry操作
+// studentEntity.save(function(err) {
+//     if (err) return console.error(err);
+//     console.log('Entiry add new success.')
+//     db.close()
 // })
 
+// add 基于modul操作
+var newStu2 = { no: 2, name: 'bbbb', age: '20' };
+// studentModel.create(newStu2, function(err){
+//     if(err) console.error(err)
+//     console.log('Model add new success.')
+//     db.close()
+// });
+
+// update
+studentModel.update(condition, update, options, callback)
+var 
+var update 
 
 
-StudentModel.find(function(err, students) {
+// find
+studentModel.find(function(err, students) {
     if (err) console.erroe(err)
     console.log({ students: students })
+    db.close()
 })
 
-StudentModel.findById('58341a8cacf92f387a5b8b07', function(err, students) {
-    if (err) console.erroe(err)
-    console.log({ student: students })
-})
+// studentModel.findById('58341a8cacf92f387a5b8b07', function(err, students) {
+//     if (err) console.erroe(err)
+//     console.log({ student: students })
+//     db.close()
+// })
